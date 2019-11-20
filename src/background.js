7248d870f80ee8891dc6a515def545e965d506b8
SJ <suresh@vicarasolutions.com>
2019-11-19 23:57:54 -0500

    Resolved issue #3: Blocked websites should not be hardcoded * Solved with an incredibly hack and slash solution (not for production), will be updated with the use of a Typescript update

--- src/background.js
+++ src/background.js
@@ -1,21 +1,39 @@
-const callback = function (details) {
-    return {
-        redirectUrl: "https://bitbucket.org/dashboard/issues"
-    };
-};
+// console.log(chrome.storage);
 
+let blockedUrls = [];
 // TODO: Hardcoded filters - these should be added by the user using the storage API
-const filter = {
+let filter = {
     urls: [
         "*://*.facebook.com/*",
         "*://*.twitter.com/*",
         "*://*.linkedin.com/*",
         "*://*.netflix.com/*",
         "*://*.tsn.ca/*",
-    ]
+    ],
 };
 
-const options = ["blocking"];
+const listener = function(details) {
+    return {
+        redirectUrl: "https://bitbucket.org/dashboard/issues",
+    };
+};
+
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+    chrome.webRequest.onBeforeRequest.removeListener(listener);
+
+    chrome.storage.local.get(["blockedUrls"], function(result) {
+        const urls = result.blockedUrls.map((url) => `*://*.${url}/*`);
+        if (urls.length === 0) {
+            return;
+        }
+
+        const filter = {
+            urls,
+        };
+        chrome.webRequest.onBeforeRequest.addListener(listener, filter, ["blocking"]);
+        console.log("Changed listener - after filter = ", filter);
+    });
+});
 
 // https://developer.chrome.com/extensions/webRequest#event-onBeforeRequest
-chrome.webRequest.onBeforeRequest.addListener(callback, filter, options);
+chrome.webRequest.onBeforeRequest.addListener(listener, filter, ["blocking"]);
--- src/popup.js
+++ src/popup.js
@@ -1,1 +1,33 @@
-// TODO
+function addToBlockList(hostname) {
+    console.log("Adding to block list:", hostname);
+    chrome.storage.local.get(["blockedUrls"], function(result) {
+        console.log("Result:", result);
+        chrome.storage.local.set({ blockedUrls: [hostname, ...result.blockedUrls] });
+    });
+}
+
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+    chrome.storage.local.get(["blockedUrls"], function(result) {
+        const blockList = this.document.getElementById("blockList");
+        blockList.value = result.blockedUrls;
+    });
+});
+
+window.onload = function() {
+    console.log(chrome.storage);
+    const urlInput = this.document.getElementById("blockUrl");
+    const query = { active: true, currentWindow: true };
+    function callback(tabs) {
+        const currentTab = tabs[0]; // there will be only one in this array
+        console.log(currentTab); // also has properties like currentTab.id
+        const url = new URL(currentTab.url);
+        urlInput.value = url.hostname;
+    }
+    chrome.tabs.query(query, callback);
+
+    const blockButton = document.getElementById("blockButton");
+    blockButton.onclick = function(element) {
+        console.log(urlInput.value);
+        addToBlockList(urlInput.value);
+    };
+};
